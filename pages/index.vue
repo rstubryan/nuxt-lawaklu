2e268fd52868f68371b22be4243f645b980cc954
rstubryan <restubumirr@gmail.com>
2024-01-05 18:04:51 +0700

    Test Pinia to show json data

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <MainLayout> Index page </MainLayout>
+  <MainLayout>
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+          <div class="text-black" v-if="isPosts" v-for="post in posts" :key="post">
+            {{ post }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "@/layouts/MainLayout.vue";
+
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+//const user = useSupabaseUser();
+
+let posts = ref([]);
+let isPosts = ref(true);
+let isLoading = ref(false);
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name: "John Doe",
+      image: "https://placehold.co/100",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      picture: "https://placehold.co/500",
+    },
+  ];
+});
 </script>
